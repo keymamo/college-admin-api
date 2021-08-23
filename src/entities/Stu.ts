b67aa7f4defbde97ddf2ffda33a31766c77e5008
Yun, Kim <brand13@naver.com>
2021-08-23 23:43:11 +0900

    add validation option

--- src/entities/Stu.ts
+++ src/entities/Stu.ts
@@ -8,16 +8,37 @@ import {
 } from "typeorm";
 import { Prof } from "./Prof";
 import { Classlist } from "./Classlist";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsOptional, IsString } from "class-validator";
 
-@Index("pid", ["pid"], {})
 @Entity("stu", { schema: "school" })
 export class Stu {
+
+  @ApiProperty({
+    example: 1,
+    description: '학생 id 번호',
+    required: true
+  })
+  @IsNumber()
   @Column("int", { primary: true, name: "id", default: () => "'0'" })
   id: number;
 
+  @ApiProperty({
+    example: '김혁신',
+    description: '학생 이름',
+    required: true
+  })
+  @IsString()
   @Column("char", { name: "name", nullable: true, length: 8 })
   name: string | null;
 
+  @ApiProperty({
+    example: 2,
+    description: '교수 id 번호',
+    required: false
+  })
+  @IsNumber()
+  @IsOptional()
   @Column("int", { name: "pid", nullable: true })
   pid: number | null;
 
