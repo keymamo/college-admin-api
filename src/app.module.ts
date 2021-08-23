375e0974e10156a639a5e99a2a16d60cf5447914
Yun, Kim <brand13@naver.com>
2021-08-23 23:41:03 +0900

    TypeOrmMoudle included

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import ormConfig from '../ormconfig';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { StudentModule } from './student/student.module';
+import { ClasslistModule } from './classlist/classlist.module';
+import { ProfessorModule } from './professor/professor.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot(ormConfig)
+    TypeOrmModule.forRoot(ormConfig),
+    StudentModule,
+    ClasslistModule,
+    ProfessorModule
   ],
   controllers: [AppController],
   providers: [AppService],
@@ -20,5 +26,4 @@ export class AppModule implements NestModule {
     // throw new Error('Method not implemented.');
     consumer.apply(LoggerMiddleware).forRoutes('*');
   }
-
 }
