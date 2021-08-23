6da42dd9a52b4368f716ac7d2ecc55920fb08d33
Yun, Kim <brand13@naver.com>
2021-08-23 23:41:57 +0900

    update for swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true
+  }));
+
+  const config = new DocumentBuilder()
+                  .setTitle('College Admin API')
+                  .setDescription('학교 관리자 페이지 개발을 위햔 API 문서입니다.')
+                  .setVersion('1.0')
+                  .addCookieAuth('connect.sid')
+                  .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   console.log(`server running at ${port}`);
 }
